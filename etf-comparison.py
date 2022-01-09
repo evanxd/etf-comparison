3d05c4c2e0b3aecc58d65b7c99c290c614b526c0
Tseng-jen (Evan) Tseng <evan@tseng.io>
2022-01-09 23:44:58 +0800

    Implement the app

---
+++ etf-comparison.py
@@ -0,0 +1,19 @@
+from datetime import date
+import streamlit as st
+
+import utils
+
+today = date.today()
+start_date = st.date_input("Start Date", today.replace(year=today.year - 10))
+end_date = st.date_input("End Date", today)
+
+data = utils.download(["VT", "SPY", "0050.TW"], start=start_date, end=end_date)
+utils.normalize(data)
+
+result = utils.merge(data)[["Close", "Close_1", "Close_2"]]
+result.rename({"Close": "VT", "Close_1": "SPY", "Close_2": "0050.TW"}, axis=1, inplace=True)
+
+st.write("""
+## VT v. SPY v. 0050.TW
+""")
+st.line_chart(result)
