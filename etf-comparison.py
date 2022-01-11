3c627919b7b2a36168097bf478a501a60350d7d7
Tseng-jen (Evan) Tseng <evan@tseng.io>
2022-01-11 17:34:37 +0800

    Add checkboxes to select tickers we want to compare

--- etf-comparison.py
+++ etf-comparison.py
@@ -1,7 +1,11 @@
 from datetime import date
+import re
 import streamlit as st
 
 import utils
+
+TICKERS = { "Global": "VT", "US": "SPY", "TW": "0050.TW" }
+tickers = [TICKERS[t] for t in TICKERS]
 
 st.write("""
 # ETF Comparison
@@ -11,13 +15,17 @@ today = date.today()
 start_date = st.date_input("Start Date", today.replace(year=today.year - 10))
 end_date = st.date_input("End Date", today)
 
-data = utils.download(["VT", "SPY", "0050.TW"], start=start_date, end=end_date)
+data = utils.download(tickers, start=start_date, end=end_date)
 utils.normalize(data)
 
 result = utils.merge(data)[["Close", "Close_1", "Close_2"]]
-result.rename({"Close": "VT", "Close_1": "SPY", "Close_2": "0050.TW"}, axis=1, inplace=True)
+result.rename({"Close": tickers[0], "Close_1": tickers[1], "Close_2": tickers[2]}, axis=1, inplace=True)
 
-st.write("""
-## VT v. SPY v. 0050.TW
-""")
-st.line_chart(result)
+selected_tickers = []
+for market in TICKERS:
+    checked = st.checkbox("{market} ({ticker})".format(market=market, ticker=TICKERS[market]), value=True)
+    if checked:
+        selected_tickers.append(TICKERS[market])
+
+if len(selected_tickers) > 0:
+    st.line_chart(result[selected_tickers])
