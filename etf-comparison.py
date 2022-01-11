aababc866841aed3501480f69e8f8d5fe2d12c6a
Tseng-jen (Evan) Tseng <evan@tseng.io>
2022-01-11 22:10:33 +0800

    Use multi-select to instead of checkboxes for picking ETFs

--- etf-comparison.py
+++ etf-comparison.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 import utils
 
-TICKERS = {"Global": "VT", "Japan": "EWJ", "Korea": "EWY", "Taiwan": "0050.TW", "United States": "SPY"}
+TICKERS = {"Global (VT)": "VT", "Japan (EWJ)": "EWJ", "Korea (EWY)": "EWY", "Taiwan (0050.TW)": "0050.TW", "United States (SPY)": "SPY"}
 tickers = [TICKERS[t] for t in TICKERS]
 
 st.write("""
@@ -26,11 +26,8 @@ for i, market in enumerate(TICKERS):
     new_names[names[i]] = market
 result.rename(new_names, axis=1, inplace=True)
 
-selected_markets = []
-for market in TICKERS:
-    checked = st.checkbox("{market} ({ticker})".format(market=market, ticker=TICKERS[market]), value=True)
-    if checked:
-        selected_markets.append(market)
+markets = [market for market in TICKERS]
+selected_markets = st.multiselect('Pick ETFs', markets, markets)
 
 if len(selected_markets) > 0:
     st.line_chart(result[selected_markets])
