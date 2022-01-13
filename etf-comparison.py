417f2d884b66a3fbfc45800e647393d01fbc8a94
Tseng-jen (Evan) Tseng <evan@tseng.io>
2022-01-13 12:07:21 +0800

    Cleanup the code

--- etf-comparison.py
+++ etf-comparison.py
@@ -1,11 +1,10 @@
 from datetime import date
-import json
 import random
 import streamlit as st
 
 import utils
 
-TICKERS = json.load(open("tickers.json", encoding="utf-8"))
+TICKERS = utils.TICKERS
 tickers = [TICKERS[t] for t in TICKERS]
 
 st.write("""
@@ -19,21 +18,10 @@ end_date = st.date_input("End Date", today)
 data = utils.download(tickers, start=start_date, end=end_date)
 utils.normalize(data)
 
-names = ["Close_{i}".format(i=i) for i in range(len(data))]
-# Workaround: The name of the first Close column is not with "_0" suffix
-# when the length of ticker list is odd.
-names[0] = "Close" if len(data) % 2 != 0 else names[0]
-result = utils.merge(data)[names]
-
-new_names = {}
-for i, market in enumerate(TICKERS):
-    new_names[names[i]] = market
-result.rename(new_names, axis=1, inplace=True)
-
-markets = [market for market in TICKERS]
 container = st.container()
 all = st.checkbox("Pick All")
 
+markets = [market for market in TICKERS]
 if "default_selected_markets" not in st.session_state:
     default_selected_markets = [markets[0]]
     default_selected_markets.extend(random.sample(markets[1:], 2))
@@ -48,5 +36,6 @@ else:
         'Pick ETFs', markets, st.session_state["default_selected_markets"]
     )
 
+result = utils.merge(data)
 if len(selected_markets) > 0:
     st.line_chart(result[selected_markets])
--- utils.py
+++ utils.py
@@ -1,12 +1,34 @@
+import json
 import pandas as pd
 import yfinance as yf
+
+TICKERS = json.load(open("tickers.json", encoding="utf-8"))
 
 
 def download(tickers, start, end):
     return [yf.download(ticker, start=start, end=end) for ticker in tickers]
 
 
-def merge(data, i=0):
+def merge(data):
+    names = ["Close_{i}".format(i=i) for i in range(len(data))]
+    # Workaround: The name of the first Close column is not with "_0" suffix
+    # when the length of ticker list is odd.
+    names[0] = "Close" if len(data) % 2 != 0 else names[0]
+    result = __merge(data)[names]
+
+    new_names = {}
+    for i, market in enumerate(TICKERS):
+        new_names[names[i]] = market
+    result.rename(new_names, axis=1, inplace=True)
+    return result
+
+
+def normalize(data):
+    for d in data:
+        d.Close = d.Close / max(d.Close)
+
+
+def __merge(data, i=0):
     suffixes = (
         "_{left_suffix}".format(left_suffix=i),
         "_{right_suffix}".format(right_suffix=i+1)
@@ -15,10 +37,5 @@ def merge(data, i=0):
         return pd.merge(data[i], data[i+1], on="Date", suffixes=suffixes)
     else:
         return pd.merge(
-            data[i], merge(data, i=(i + 1)), on="Date", suffixes=suffixes
+            data[i], __merge(data, i=(i + 1)), on="Date", suffixes=suffixes
         )
-
-
-def normalize(data):
-    for d in data:
-        d.Close = d.Close / max(d.Close)
